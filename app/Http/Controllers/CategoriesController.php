3eb9c488ed419619b730b8a60974262d78bd6d65
TăHā Ăllëkssăndȅr <tkhalainou@gmail.com>
2020-11-09 09:30:56 +0100

    new changes

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -4,15 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\Categories;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class CategoriesController extends Controller
 {
     
+    public function __construct(){
+
+      $this-> middleware('auth');
+    }
+
     public function index()
     {
       $cats = Categories::select('id', 'name', 'description')->get();
+
       return view('categories.show')->with('cats', $cats);
+      
     }
 
    
